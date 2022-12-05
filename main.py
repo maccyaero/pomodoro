1c35017e2f47bf4612211ba4725db3463a15704a
maccyaero <17176445+maccyaero@users.noreply.github.com>
2022-12-05 13:16:34 +1100

    Added Pomodoro Timer Mechanism such that count down timer starts with 25 mins then takes 5 mins break. Repeats this behaviour 7 times and then gives a 20 mins break.

--- main.py
+++ main.py
@@ -10,40 +10,38 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
-break_time = 0
+reps = 0
 
 
 # ---------------------------- TIMER RESET ------------------------------- #
 
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 def start_timer():
-    seconds = 1
-    countdown(seconds)
+    global reps
+    reps += 1
+    work_seconds = WORK_MIN * 60
+    short_break_seconds = SHORT_BREAK_MIN * 60
+    long_break_seconds = LONG_BREAK_MIN * 60
+
+    if reps % 8 == 0:  # If it is the 8th rep
+        countdown(long_break_seconds)
+    elif reps % 2 == 0:  # If it s the 2nd, 4th or 6th rep
+        countdown(short_break_seconds)
+    else:  # If it s the 1st, 3rd, 5th or 7th rep
+        countdown(work_seconds)
 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 def countdown(count):
     minutes = math.floor(count / 60)
     seconds = count % 60
-    global break_time
-
     if seconds < 10:
         seconds = f"0{seconds}"
     canvas.itemconfig(time_text, text=f"{minutes}:{seconds}")
-    if count > 0 and break_time == 0:
-        if count == 1:
-            break_time = 1
-            countdown(5)
-        else:
-            window.after(1000, countdown, count - 1)
-
-    if count > 0 and break_time == 1:
-        if count == 1:
-            break_time = 0
-            countdown(10)
-        else:
-            window.after(1000, countdown, count - 1)
-
+    if count > 0:
+        window.after(1000, countdown, count - 1)
+    else:
+        start_timer()
 
 
 # ---------------------------- UI SETUP ------------------------------- #
